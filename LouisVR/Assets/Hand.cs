813beabfb4ff9acbd8718333150c5e511a272367
Louis <louis_foy@yahoo.co.uk>
2018-11-15 11:37:40 +0000

    - Added VR project starter - Example city - Hand movement - Hand gripping

---
+++ LouisVR/Assets/Hand.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+    private Camera camera;
+
+    private bool isGripping = false;
+
+    private Vector3 previousPosition;
+
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Update drag movement
+        UpdateDragMovement();
+
+        // Update the previous position
+        previousPosition = transform.position;
+
+        // Position the hand from camera
+        Ray cameraRay = camera.ScreenPointToRay(Input.mousePosition);
+        float maxArmLength = 2.0f;
+        RaycastHit hit;
+
+        if (Physics.Raycast(cameraRay, out hit, maxArmLength, ~(1<<LayerMask.NameToLayer("Hand"))))
+        {
+            Debug.Log("Collided with " + hit.collider.gameObject);
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = cameraRay.origin + cameraRay.direction * maxArmLength;
+        }
+
+        // Drag
+        isGripping = Input.GetButton("Fire1");
+	}
+
+    void UpdateDragMovement()
+    {
+        if (isGripping)
+        {
+            // Pull the camera along
+            camera.transform.position -= transform.position - previousPosition;
+        }
+    }
+}
---
+++ LouisVR/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move with WASD
+        transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime;
+    }
+}
